2b2c6d179e6228ccccc39ec595b0e8c746cd0add
Zachary Kline <zkline@esri.com>
2021-02-18 11:49:03 -0500

    move parenthesis

--- Package.swift
+++ Package.swift
@@ -16,4 +16,5 @@ let package = Package(
             url: "https://www.dropbox.com/s/rsr5uj7tfyl54nj/ArcGIS-v100.10.xcframework.zip?dl=1",
             checksum: "e53bd68b20cab696c4d8a8b076c6122f4357e4891c26f7dfc3b2351850f2d7de"
         )
-    ])
+    ]
+)
